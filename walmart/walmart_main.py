dcde57c9dc625245f404c5a4ffed587c1df35038
SatyaDivya <divya.maddipudi1@gmail.com>
2019-04-17 23:36:10 +0530

    200 images added

--- walmart/walmart_main.py
+++ walmart/walmart_main.py
@@ -8,19 +8,20 @@ def make_soup(url):
     soupdata = BeautifulSoup(page, "html.parser")
     return soupdata
 
-i = 556
+i = 181
 
-soup = make_soup("https://www.walmart.com/search/?query=laundry%20products&cat_id=0&typeahead=laundry%20pr"+"?page=4")
+soup = make_soup("https://www.walmart.com/browse/3944_1060825_3289709_1180168?povid=447913+%7C+contentZone1+%7C+2017-07-24+%7C+1+%7C+Perfection_AllTVs_TVs_By_4K")
 
 for img in soup.findAll('img'):
     temp = img.get('data-image-src')
     #print(temp)
 
-    if temp != None:
+    if temp != None and i <=200:
         image = temp
         print(image)
 
         
+                
         filename = str(i)
         i = i + 1    
 
